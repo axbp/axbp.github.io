e2417608f52dcc81c4016c9515d5197b7f40ebcc
axbp <feniate@example.com>
2022-10-19 16:42:27 +0200

    commit

--- script.js
+++ script.js
@@ -1,5 +1,2 @@
-const element = document.getElementById('middle');
-const elementRect = element.getBoundingClientRect();
-const absoluteElementTop = elementRect.top + window.pageYOffset;
-const middle = absoluteElementTop - (window.innerHeight / 2);
-window.scrollTo(0, middle);
+document.body.scrollIntoView({block: "center"});
+document.body.style.overflow = "hidden"
